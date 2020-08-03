ffdc83cef138ebfd9fe1360ee9331d15ad490899
materiaIce <rioriorio049@gmail.com>
2020-08-03 15:13:22 +0900

    update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$(document).on('click','.jump',function(event){
+  event.preventDefault();
+  var target=$(this).attr("href");
+  $('html,body').animate({
+    scrollTop: $(target).offset().top
+  },500);
+});
+
 var f=false;
 $(".topcircle").click(function(){
   if(f=!f){
@@ -7,12 +15,4 @@ $(".topcircle").click(function(){
     $('.topcircle').css("background-color","#62b6cb");
     $('.block').hide(300)
   }
-});
-
-$(document).on('click','.jump',function(event){
-  event.preventDefault();
-  var target=$(this).attr("href");
-  $('html,body').animate({
-    scrollTop: $(target).offset().top
-  },500);
 });
